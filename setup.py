8e6e779830261066c01a4e5bfe4427306948713f
Chris Lo <chris@mytaiwantour.com>
2020-02-25 14:27:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,28 @@ from setuptools import setup
 with open('tappay/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
-setup(name='tappay',
-      version=version,
-      description='TapPay Client Library for Python',
-      long_description='This is the Python client library for TapPay\'s API. To use it you\'ll need a TapPay account. Sign up `for free at tappaysdk.com <https://portal.tappaysdk.com/register?src=python-client-library>`_.',
-      url='http://github.com/shihweilo/tappay-python',
-      author='Shihwei Lo',
-      author_email='shihwei@gmail.com',
-      license='MIT',
-      packages=['tappay'],
-      platforms=['any'],
-      install_requires=['requests'],
-      classifiers=[
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ])
+setup(
+    name='tappay',
+    version=version,
+    description='TapPay Client Library for Python',
+    long_description='This is the Python client library for TapPay\'s API. To use it you\'ll need a TapPay account. Sign up `for free at tappaysdk.com <https://portal.tappaysdk.com/register?src=python-client-library>`_.',
+    url='http://github.com/shihweilo/tappay-python',
+    author='Shihwei Lo',
+    author_email='shihwei@gmail.com',
+    license='MIT',
+    packages=['tappay'],
+    platforms=['any'],
+    install_requires=['requests>=2.4.2'],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
